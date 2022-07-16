e2f0118ec4a39fee1ffde005e905842ede5bd4a8
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-07-16 13:42:29 +0100

    first task on js

---
+++ javascript.js/lesson1.js
@@ -0,0 +1,16 @@
+// javascript lessons
+
+// performing a function that interchange variables
+function test () {
+    var a = 3;
+    var b = 8;
+
+    // solution
+    var c = a;
+    a = b;
+    b = c;
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+test ();
