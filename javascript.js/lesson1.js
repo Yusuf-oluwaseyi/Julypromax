24b1b21f9f788cf7b9b0938509753637c6cd111b
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-07-18 14:31:28 +0100

    bottle task

--- javascript.js/lesson1.js
+++ javascript.js/lesson1.js
@@ -162,3 +162,18 @@ function FizzBuzz() {
 
 }
 FizzBuzz();
+
+// printing bottle challenge
+
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
