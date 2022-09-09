872cf2497e07dab225860cda443634b8c996582e
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-09-09 15:10:42 +0100

    Finishing the production section

--- July/index.js
+++ July/index.js
@@ -13,3 +13,25 @@ if (close) {
         nav.classList.remove('active')
     })
 }
+
+// sproduct script
+
+var mainImg = document.getElementById("MainImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].addEventListener('click', function () {
+    mainImg.src = smallImg[0].src;
+});
+smallImg[1].addEventListener('click', function () {
+    mainImg.src = smallImg[1].src;
+});
+smallImg[2].addEventListener('click', function () {
+    mainImg.src = smallImg[2].src;
+});
+smallImg[3].addEventListener('click', function () {
+    mainImg.src = smallImg[3].src;
+});
+smallImg[4].addEventListener('click', function () {
+    mainImg.src = smallImg[4].src;
+});
+    
